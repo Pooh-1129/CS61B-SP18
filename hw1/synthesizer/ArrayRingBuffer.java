9fc240fbc67851a6ca26f27bceea179aed219b90
Pooh-1129 <1033191961@qq.com>
2022-10-24 20:00:04 +0800

    revise hw1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -56,7 +56,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
         }
         T tmp = rb[first];
         first = (first + 1) % capacity;
-        fillCount++;
+        fillCount--;
         return tmp;
     }
 
@@ -66,6 +66,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer empty");
+        }
         return rb[first];
     }
 
