c809101b1d27414924a675f2589f15ef64757e6e
Pooh-1129 <1033191961@qq.com>
2022-11-29 18:50:06 +0800

    finish lab13

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -17,7 +17,16 @@ public class RadixSort {
      */
     public static String[] sort(String[] asciis) {
         // TODO: Implement LSD Sort
-        return null;
+        int max = Integer.MIN_VALUE;
+        for (String s : asciis) {
+            max = max > s.length() ? max : s.length();
+        }
+        String[] sorted = new String[asciis.length];
+        System.arraycopy(asciis, 0, sorted, 0, asciis.length);
+        for (int t = max - 1; t >= 0; t--) {
+            sorted = sortHelperLSD(sorted, t);
+        }
+        return sorted;
     }
 
     /**
@@ -26,9 +35,38 @@ public class RadixSort {
      * @param asciis Input array of Strings
      * @param index The position to sort the Strings on.
      */
-    private static void sortHelperLSD(String[] asciis, int index) {
+    private static String[] sortHelperLSD(String[] asciis, int index) {
         // Optional LSD helper method for required LSD radix sort
-        return;
+        int[] counts = new int[256];
+        int[] starts = new int[256];
+
+        for (String s : asciis) {
+            if (s.length() - 1 < index) {
+                counts[0] += 1;
+            }
+            else {
+                counts[s.charAt(index)] += 1;
+            }
+        }
+        int pos = 0;
+        for (int i = 0; i< starts.length; i++ ) {
+            starts[i] = pos;
+            pos += counts[i];
+        }
+        String[] sorted = new String[asciis.length];
+        for (String s : asciis) {
+            int p = 0;
+            if (s.length() - 1 < index) {
+                p = counts[0];
+                starts[0] += 1;
+            }
+            else {
+                p = counts[s.charAt(index)];
+                starts[s.charAt(index)] += 1;
+            }
+            sorted[p] = s;
+        }
+        return sorted;
     }
 
     /**
